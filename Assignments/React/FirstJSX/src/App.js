01c712473c55a995f32d9772430b922a8efa9951
Imas Zubaidia <imassamirammal97@gmail.com>
2022-12-07 06:08:38 +0200

    FirstJSX

---
+++ Assignments/React/FirstJSX/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="first">
+    <h1>Hello Dojo!</h1>
+    <h5>Things I need to do:</h5>
+    <ul>
+      <li>Learn react</li>
+      <li>Climb MT Evarest</li>
+      <li>Run a marathon</li>
+      <li>feed th dogs</li>
+    </ul>
+  </div>
+  );
+}
+
+export default App;
